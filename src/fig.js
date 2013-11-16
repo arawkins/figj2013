0d2fcca0d014c804a871073098e96dc9f4cfd469
Drew Miller <drew666@gmail.com>
2013-11-15 23:26:39 -0800

    hope

--- src/fig.js
+++ src/fig.js
@@ -7,6 +7,27 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+
+// Cube idea
+// Create an array of materials to be used in a cube, one for each side
+var cubeMaterialArray = [];
+// order to add materials: x+,x-,y+,y-,z+,z-
+cubeMaterialArray.push( new THREE.MeshBasicMaterial( { color: 0xff3333 } ) );
+cubeMaterialArray.push( new THREE.MeshBasicMaterial( { color: 0xff8800 } ) );
+cubeMaterialArray.push( new THREE.MeshBasicMaterial( { color: 0xffff33 } ) );
+cubeMaterialArray.push( new THREE.MeshBasicMaterial( { color: 0x33ff33 } ) );
+cubeMaterialArray.push( new THREE.MeshBasicMaterial( { color: 0x3333ff } ) );
+cubeMaterialArray.push( new THREE.MeshBasicMaterial( { color: 0x8833ff } ) );
+var cubeMaterials = new THREE.MeshFaceMaterial( cubeMaterialArray );
+// Cube parameters: width (x), height (y), depth (z),
+//        (optional) segments along x, segments along y, segments along z
+var cubeGeometry = new THREE.CubeGeometry( 100, 100, 100, 1, 1, 1 );
+// using THREE.MeshFaceMaterial() in the constructor below
+//   causes the mesh to use the materials stored in the geometry
+cube = new THREE.Mesh( cubeGeometry, cubeMaterials );
+cube.position.set(-60, 60, camera.position.z - 500);
+scene.add( cube );
+
 	
 // FLOOR
 	var floorTexture = new THREE.ImageUtils.loadTexture( 'gfx/checkerboard.png' );
@@ -56,6 +77,7 @@ function render() {
 		grid.unshift(shuffle(newRow));
 	}
 */
+
 	
 	if (floor.position.z- floorHalfHeight > camera.position.z) {
 		//console.log(floor.position.z);
