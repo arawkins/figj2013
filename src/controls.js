9a49273e9654b11c3ee20bcbe204b88b8d0aef5d
arawkins <arawkins@gmail.com>
2013-11-15 23:25:21 -0800

    updated controls + bluegrid

--- src/controls.js
+++ src/controls.js
@@ -1,16 +1,19 @@
 var controls = function (camera) {
-	var scrollSpeed = 0.5;
+	var scrollSpeed = 5;
 	var cameraVX = 0;
 	var cameraVY = 0;
-	var cameraAccel = 0.05;
+	var cameraAccel = 5;
 	var cameraMaxVelocity = 0.25;
 	var cameraMinAltitude = 3;
-	var rollSpeed = 0.01;
-
+	var rollSpeed = 0.03;
+	var maxRoll = 0.7;
+	
 	if (Key.isDown(Key.LEFT)) {
 		cameraVX -= cameraAccel;
 		if (cameraVX < -cameraMaxVelocity) cameraVX = -cameraMaxVelocity;
-		//camera.rotation.z += rollSpeed;
+		camera.rotation.z += rollSpeed;
+		console.log(camera.rotation.z);
+		if (camera.rotation.z > maxRoll) camera.rotation.z = maxRoll;
 	} else {
 		if (camera.rotation.z > 0) camera.rotation.z -= rollSpeed;
 		if (cameraVX < 0 ) {
--- src/fig.js
+++ src/fig.js
@@ -15,14 +15,23 @@ document.body.appendChild( renderer.domElement );
 	var floorMaterial = new THREE.MeshBasicMaterial( { map: floorTexture, side: THREE.DoubleSide } );
 	var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
 	var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+	var floor2 = new THREE.Mesh(floorGeometry, floorMaterial);
+	var floorHalfHeight = floor.geometry.height/2;
+	
 	floor.position.y = -20;
 	floor.rotation.x = Math.PI / 2;
+	
+	floor2.position.y = floor.position.y;
+	floor2.rotation.x = floor.rotation.x;
+	floor2.position.z = -floor.geometry.height;
+	
 	scene.add(floor);
-
+	scene.add(floor2);
+	
 function init() {
 
 	camera.position.z = 30;
-	camera.position.y = 4;
+	camera.position.y = 25;
 	//camera.position.x = (gridWidth + gridWidth*cubePadding)/2;
 	window.addEventListener('keyup', function(event) { Key.onKeyup(event); }, false);
 	window.addEventListener('keydown', function(event) { Key.onKeydown(event); }, false);
@@ -48,6 +57,14 @@ function render() {
 	}
 */
 	
+	if (floor.position.z- floorHalfHeight > camera.position.z) {
+		//console.log(floor.position.z);
+		floor.position.z -= floor.geometry.height*2;
+	} 
+
+	if (floor2.position.z- floorHalfHeight > camera.position.z) {
+		floor2.position.z -= floor2.geometry.height*2;
+	}
 	requestAnimationFrame(render);
 	renderer.render(scene, camera);
 }
