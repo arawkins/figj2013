0a9982c16b6644f03f7598849db69fc6e011b9f7
arawkins <arawkins@gmail.com>
2013-11-16 19:45:52 -0800

    Added new collision system

--- src/obstacles.js
+++ src/obstacles.js
@@ -20,8 +20,7 @@ var obstacles = (function () {
 	function tick() {
 		frameCounter = frameCounter + 1;
 		tickCounter++;
-		//console.log(tickCounter);
-		if (tickCounter > 3) {
+		if (tickCounter > 2) {
 			tickCounter = 0;
 			addBox();
 		}
@@ -29,8 +28,7 @@ var obstacles = (function () {
 		for (var i=0;i<cubes.length;i++) {
 			var thisCube = cubes[i];
 			if (thisCube.position.z > camera.position.z) {
-				console.log(cubes.length);
-				cubes.splice(i,1);
+				cubes.splice(i,5);
 				scene.remove(thisCube);
 				oldCubes.push(thisCube);
 			}
@@ -39,24 +37,67 @@ var obstacles = (function () {
 	};
 
 	function addBox() {
-		console.log('addbox');
 		
 		var cube;
 		if (oldCubes.length > 0) 
 			cube = oldCubes.pop();
 		else 		
-			cube = objects.makeCube({x:100, y:100, z:100});
+			cube = objects.makeCube({x:getRandomInt(100,500), y:getRandomInt(50,500), z:getRandomInt(50,500)});
 			
 		var newX = getRandomInt(-2000,2000);
-		var newY = 60;
-		var newZ = getRandomInt(-1900,-2900) + camera.position.z;
+		//var newX = 0;
+		var newY = getRandomInt(0,1000);
+		var newZ = getRandomInt(-4900,-5900) + camera.position.z;
 		cube.position.set(newX, newY, newZ);
+		cube.geometry.computeBoundingBox();
 		scene.add(cube);
 		//collidableMeshList.push(cube);
 		cubes.push(cube);
 	}
 
 	function collide(obj) {
+		for (var i=0;i<cubes.length;i++) {
+			var cube = cubes[i];
+			var bb = cube.geometry.boundingBox;
+			var cx = cube.position.x;
+			var ox = obj.position.x;
+			var cz = cube.position.z;
+			var oz = obj.position.z;
+			var cy = cube.position.y;
+			var oy = obj.position.y;
+			
+			//console.log('oz:', oz);
+			//console.log('cz:', cz);
+			/*console.log(cz + bb.min.z);
+			console.log(cz + bb.max.z);
+			console.log(cz);
+			console.log(bb.min.z);
+			console.log(bb.max.z);
+			console.log('-------');
+			*/
+			//console.log('ox',ox,'leftx',cx + bb.min.x,'rightx',cx + bb.max.x);
+			if(oz < cz + bb.max.z) {
+				if(ox > cx + bb.min.x && ox < cx + bb.max.x) {
+					if(oy > cy + bb.min.y && oy < cy + bb.max.y) {
+						console.log('hit');
+					}
+				}
+				
+				/*
+				if(ox > cx - bb.min.x || ox < cx + bb.max.x) {
+					if(oy > cy - bb.min.y || oy < cy + bb.max.y) {
+						return true;
+					}
+				}*/
+			}	
+			//console.log(bb.min);
+			//console.log(bb.max);
+			//if (bb.z > obj.position.z) {
+				
+			//}
+			
+		}
+		/*
 		var originPoint = obj.position.clone();
 
 		for (var vertexIndex = 0; vertexIndex < obj.geometry.vertices.length; vertexIndex++) {
@@ -72,6 +113,7 @@ var obstacles = (function () {
 				}
 			}
 		}
+		*/
 	};
 
 	return {
