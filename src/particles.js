56cbf95de2baeec672fae84c631421e767115fab
Drew Miller <drew666@gmail.com>
2013-11-17 11:19:00 -0800

    particles skeleton

---
+++ src/particles.js
@@ -0,0 +1,91 @@
+var objects = (function () {
+
+	var clock = new THREE.Clock();
+
+	function init() {
+		addFlare();
+	};
+
+	function addFlare() {
+	
+		this.particleGeometry = new THREE.Geometry();
+			for (var i = 0; i < 100; i++)
+				particleGeometry.vertices.push( new THREE.Vector3(0,0,0) );
+			
+			var discTexture = THREE.ImageUtils.loadTexture( 'gfx/disc.png' );
+			
+			// properties that may vary from particle to particle. 
+			// these values can only be accessed in vertex shaders! 
+			//  (pass info to fragment shader via vColor.)
+			this.attributes = 
+			{
+				customColor:	 { type: 'c',  value: [] },
+				customOffset:	 { type: 'f',  value: [] },
+			};
+			
+			var particleCount = particleGeometry.vertices.length
+			for( var v = 0; v < particleCount; v++ ) 
+			{
+				attributes.customColor.value[ v ] = new THREE.Color().setHSL( 1 - v / particleCount, 1.0, 0.5 );
+				attributes.customOffset.value[ v ] = 6.282 * (v / particleCount); // not really used in shaders, move elsewhere
+			}
+			
+			// values that are constant for all particles during a draw call
+			this.uniforms = 
+			{
+				time:      { type: "f", value: 1.0 },
+				texture:   { type: "t", value: discTexture },
+			};
+
+			var shaderMaterial = new THREE.ShaderMaterial( 
+			{
+				uniforms: 		uniforms,
+				attributes:     attributes,
+				vertexShader:   document.getElementById( 'vertexshader' ).textContent,
+				fragmentShader: document.getElementById( 'fragmentshader' ).textContent,
+				transparent: true, // alphaTest: 0.5,  // if having transparency issues, try including: alphaTest: 0.5, 
+				// blending: THREE.AdditiveBlending, depthTest: false,
+				// I guess you don't need to do a depth test if you are alpha blending
+				// 
+			});
+
+			var particleCube = new THREE.ParticleSystem( particleGeometry, shaderMaterial );
+			particleCube.position.set(0, 85, 0);
+			particleCube.dynamic = true;
+			// in order for transparency to work correctly, we need sortParticles = true.
+			//  but this won't work if we calculate positions in vertex shader,
+			//  so positions need to be calculated in the update function,
+			//  and set in the geometry.vertices array
+			particleCube.sortParticles = true;
+			scene.add( particleCube );
+	};
+
+	function tick() {
+			var t0 = clock.getElapsedTime();
+			uniforms.time.value = 0.125 * t0;
+
+			for( var v = 0; v < particleGeometry.vertices.length; v++ ) 
+			{
+				var timeOffset = uniforms.time.value + attributes.customOffset.value[ v ];
+				particleGeometry.vertices[v] = position(timeOffset);		
+			}
+	};
+
+	function position(t) {
+		// x(t) = cos(2t)*(3+cos(3t))
+		// y(t) = sin(2t)*(3+cos(3t))
+		// z(t) = sin(3t)
+		return new THREE.Vector3(
+			20.0 * Math.cos(2.0 * t) * (3.0 + Math.cos(3.0 * t)),
+			20.0 * Math.sin(2.0 * t) * (3.0 + Math.cos(3.0 * t)),
+			50.0 * Math.sin(3.0 * t) );
+	};
+
+	return {
+		init: init,
+		tick: tick
+	};
+
+})()
+
+
