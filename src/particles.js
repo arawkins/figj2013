d550bd3df541853e02437cfb192a167a05a75c8f
Drew Miller <drew666@gmail.com>
2013-11-17 16:27:38 -0800

    particle smoke

--- src/particles.js
+++ src/particles.js
@@ -6,12 +6,28 @@ var particles = (function () {
 	var scene;
 
 	var collection = [];
+	var engine;
 
 	function init(_scene, _camera, _spaceship) {
 		scene = _scene;
 		camera = _camera;
 		spaceship = _spaceship;
+
 	};
+
+	function startSmoke() {
+		engine = new ParticleEngine();
+		var smokeValues = Examples.smoke;
+		smokeValues.positionBase.z = camera.position.z - 2400;
+		console.log(smokeValues.positionBase.x, smokeValues.positionBase.y, smokeValues.positionBase.z);
+		engine.setValues(smokeValues);
+		engine.initialize();
+		scene.add(engine.particleMesh);
+	}
+
+	function stopSmoke() {
+		scene.remove(engine.particleMesh);
+	}
 
 	function getFlare() {
 		var f = new Flare();
@@ -20,14 +36,14 @@ var particles = (function () {
 	};
 
 	function Flare() {
-	
+
 		this.particleGeometry = new THREE.Geometry();
 		for (var i = 0; i < 10; i++) {
 			this.particleGeometry.vertices.push( new THREE.Vector3(0,0,0) );
 		}
-			
+
 		var discTexture = THREE.ImageUtils.loadTexture( 'gfx/disc.png' );
-		
+
 		// properties that may vary from particle to particle. 
 		// these values can only be accessed in vertex shaders! 
 		//  (pass info to fragment shader via vColor.)
@@ -35,14 +51,14 @@ var particles = (function () {
 			customColor:	 { type: 'c',  value: [] },
 			customOffset:	 { type: 'f',  value: [] },
 		};
-		
+
 		var particleCount = this.particleGeometry.vertices.length
 		for( var v = 0; v < particleCount; v++ ) {
 			this.attributes.customColor.value[ v ] = new THREE.Color().setHSL( 7.0, 0.8 - v / particleCount, 0.5 );
-      //this.attributes.customColor.value[ v ] = new THREE.Color("rgb(225,225,225)");
+			//this.attributes.customColor.value[ v ] = new THREE.Color("rgb(225,225,225)");
 			this.attributes.customOffset.value[ v ] = 6.282 * (v / particleCount); // not really used in shaders, move elsewhere
 		}
-		
+
 		// values that are constant for all particles during a draw call
 		this.uniforms = {
 			time:      { type: "f", value: 1.0 },
@@ -50,7 +66,7 @@ var particles = (function () {
 		};
 
 		var shaderMaterial = new THREE.ShaderMaterial( {
-			uniforms: 		  this.uniforms,
+			uniforms:				this.uniforms,
 			attributes:     this.attributes,
 			vertexShader:   document.getElementById( 'vertexshader' ).textContent,
 			fragmentShader: document.getElementById( 'fragmentshader' ).textContent,
@@ -61,18 +77,24 @@ var particles = (function () {
 		});
 
 		this.tick = function () {
+			var dt = clock.getDelta();
+			engine.update( dt * 0.5 );
+			return;
+			/*
 			var t0 = clock.getElapsedTime();
 			this.uniforms.time.value = 0.125 * t0;
 
 			for( var v = 0; v < this.particleGeometry.vertices.length; v++ ) {
-				var timeOffset = this.uniforms.time.value + this.attributes.customOffset.value[ v ];
-				this.particleGeometry.vertices[v] = position(timeOffset);		
+			var timeOffset = this.uniforms.time.value + this.attributes.customOffset.value[ v ];
+			this.particleGeometry.vertices[v] = position(timeOffset);		
 			}
 
 			particleCube.position.y = camera.position.y + 100;
 			particleCube.position.z = spaceship.position.z - 260;
+			*/
 		};
 
+		/*
 		var particleCube = new THREE.ParticleSystem( this.particleGeometry, shaderMaterial );
 		particleCube.position.set(spaceship.position.x, spaceship.position.y, spaceship.position.z - 10);
 		particleCube.dynamic = true;
@@ -82,36 +104,41 @@ var particles = (function () {
 		//  and set in the geometry.vertices array
 		particleCube.sortParticles = true;
 		scene.add( particleCube );
+		*/
 
 		return this;
 	};
 
 	function tick() {
-		for (var i=0; i<collection.length; i++) {
-			collection[i].tick();
-		}
+		var dt = clock.getDelta();
+		engine.update( dt * 0.5 );
+		return;
+		//for (var i=0; i<collection.length; i++) {
+			//collection[i].tick();
+		//}
 	};
 
 	function position(t) {
 		// x(t) = cos(2t)*(3+cos(3t))
 		// y(t) = sin(2t)*(3+cos(3t))
 		// z(t) = sin(3t)
-    return new THREE.Vector3(
-      20.0 * Math.cos(2.0 * t),
-      20.0 * Math.sin(2.0 * t),
-      50.0 * Math.sin(3.0 * t) );
-    /*
 		return new THREE.Vector3(
+			20.0 * Math.cos(2.0 * t),
+			20.0 * Math.sin(2.0 * t),
+			50.0 * Math.sin(3.0 * t) );
+			/*
+			return new THREE.Vector3(
 			20.0 * Math.cos(2.0 * t) * (3.0 + Math.cos(3.0 * t)),
 			20.0 * Math.sin(2.0 * t) * (3.0 + Math.cos(3.0 * t)),
 			50.0 * Math.sin(3.0 * t) );
-    */
+			*/
 	};
 
 	return {
 		init: init,
 		tick: tick,
-		getFlare: getFlare
+		getFlare: getFlare,
+		startSmoke: startSmoke
 	};
 
 })()
